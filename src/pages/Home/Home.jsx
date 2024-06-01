91ad8e0cf7dc734ef500d735f47d7c0de1506724
Esther J. Iyege <faithesther20@gmail.com>
2024-06-01 14:06:58 +0100

    added to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import Categories from './Categories'
+import TodayProduct from './TodayProduct'
 
 const Home = () => {
   return (
-    <div>
+    <div  className=" container mx-auto px-3 md:px-5">
     <Header />
     <Categories />
+    <TodayProduct />
       
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+       primary: '#DB4444', // Add your custom color here
+      },
+    }
   },
   plugins: [],
 }
