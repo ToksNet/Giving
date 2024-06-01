184cf20dbff9e53889d86f9493f6b8f51d75f2aa
Johnnyxcode <john.ayo.jam@gmail.com>
2024-06-01 07:04:53 +0100

    app

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
+import SaleBanner from "../components/SaleBanner";
 
 const MainLayout = () => {
   return (
     <>
       {" "}
       <div className="flex flex-col min-h-screen">
-        <SalesBanner />
+        <SaleBanner />
         <NavBar />
 
         <Footer />
