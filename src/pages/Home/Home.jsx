0490dd911af904bcb34f6e65c0065cca0c6825eb
Johnnyxcode <john.ayo.jam@gmail.com>
2024-06-03 09:09:53 +0100

    home arangement

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,31 +5,25 @@ import CustomerService from "./CustomerService";
 import Advert from "./Advert";
 import NewAdvert from "./NewAdvert";
 import BeautySales from "../../components/BeautySales";
-<<<<<<< HEAD
 import ExploreProduct from "../../components/ExploreProduct";
 import ExploreProduct1 from "../../components/ExploreProduct1";
 import NewArrival from "../../components/NewArrival";
 import BlackFriday from "../../components/BlackFriday";
 import TimerBanner from "../../components/TimerBanner";
-=======
 import ExploreProduct from "../components/ExploreProduct";
->>>>>>> 523c548 (banner)
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Categories />
-<<<<<<< HEAD
       <TimerBanner />
       <NewAdvert />
       <BlackFriday />
       <ExploreProduct1 />
       <NewAdvert />
       <NewArrival />
-=======
       <ExploreProduct />
->>>>>>> 523c548 (banner)
       <BeautySales />
 
       <ExploreProduct />
