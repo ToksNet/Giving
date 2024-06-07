91bfb918270f0b9e1db8f573a7c63531499ea9b6
Johnnyxcode <john.ayo.jam@gmail.com>
2024-06-07 15:19:07 +0100

    still under construction

--- src/components/ExploreProduct1.jsx
+++ src/components/ExploreProduct1.jsx
@@ -2,36 +2,55 @@ import React from "react";
 import { IoMdHeartEmpty, IoMdStar } from "react-icons/io";
 import { IoEyeOutline } from "react-icons/io5";
 
-const ProductCard = ({ imageSrc, title, price, rating, reviews }) => {
+const ProductCard = ({
+  imageSrc,
+  title,
+  price,
+  originalPrice,
+  rating,
+  reviews,
+}) => {
   return (
-    <div className="w-full md:w-52">
-      <div className="flex flex-col items-start py-2 pb-6 justify-between bg-gray-200 h-full">
-        <div className="flex justify-between w-full px-2">
-          <img src={imageSrc} className="w-28 mt-5 pl-7" alt={title} />
-          <div className="flex flex-col gap-2 items-center">
-            <a href="#">
-              <IoMdHeartEmpty className="p-1 w-7 h-7 bg-white rounded-full" />
-            </a>
-            <a href="#">
-              <IoEyeOutline className="p-1 w-7 h-7 bg-white rounded-full" />
-            </a>
+    <div className="w-270px group h-350px md:w-full md:max-w-[270px] md:h-[350px] relative">
+      <div className="flex flex-col justify-between bg-gray-200 h-250px w-270px md:h-[250px] md:w-[270px]">
+        <div>
+          <div className="relative h-180px w-190px md:h-[180px] md:w-[190px] mx-auto">
+            <img
+              src={imageSrc}
+              className="w-full h-full object-cover"
+              alt={title}
+            />
+          </div>
+          <div className=" bottom-0 left-0 w-full bg-black text-white text-center p-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            Add to Cart
           </div>
         </div>
-        <div className="mt-4 px-2 w-full">
-          <p className="text-sm font-medium">{title}</p>
-          <div className="flex gap-2 mt-1 items-center">
-            <p className="text-sm font-medium text-orange-700">{price}</p>
-            <div className="flex">
-              {Array.from({ length: 5 }, (_, index) => (
-                <IoMdStar
-                  key={index}
-                  className={`w-5 h-5 ${
-                    index < rating ? "text-orange-400" : "text-gray-300"
-                  }`}
-                />
-              ))}
+        <div>
+          <div className="mt-4 px-2 flex-grow flex flex-col justify-end bg-white h-70px w-full md:h-[70px]">
+            <p className="text-sm md:text-base lg:text-lg font-medium">
+              {title}
+            </p>
+            <div className="block">
+              <p className="text-sm font-medium text-orange-700 pr-2 inline">
+                {price}
+              </p>
+              <p className="text-sm font-medium text-gray-500 inline line-through">
+                {originalPrice}
+              </p>
             </div>
-            <p className="text-sm">({reviews})</p>
+            <div className="flex gap-2 mt-1 items-center justify-between">
+              <div className="flex">
+                {Array.from({ length: 5 }, (_, index) => (
+                  <IoMdStar
+                    key={index}
+                    className={`w-5 h-5 ${
+                      index < rating ? "text-orange-400" : "text-gray-300"
+                    }`}
+                  />
+                ))}
+              </div>
+              <p className="text-sm">({reviews})</p>
+            </div>
           </div>
         </div>
       </div>
@@ -45,6 +64,7 @@ const ExploreProduct1 = () => {
       imageSrc: "../images/Explore-1.jpg",
       title: "Breed Dry Dog Food",
       price: "$100",
+      originalPrice: "$120",
       rating: 3,
       reviews: 35,
     },
@@ -52,6 +72,7 @@ const ExploreProduct1 = () => {
       imageSrc: "../images/Explore-2.png",
       title: "CANON EOS DSLR Camera",
       price: "$360",
+      originalPrice: "$400",
       rating: 4,
       reviews: 95,
     },
@@ -59,6 +80,7 @@ const ExploreProduct1 = () => {
       imageSrc: "../images/Explore-3.png",
       title: "ASUS FHD Gaming Laptop",
       price: "$700",
+      originalPrice: "$800",
       rating: 5,
       reviews: 325,
     },
@@ -66,6 +88,7 @@ const ExploreProduct1 = () => {
       imageSrc: "../images/Explore-1.jpg",
       title: "Curology Product Set",
       price: "$500",
+      originalPrice: "$600",
       rating: 4,
       reviews: 145,
     },
@@ -73,6 +96,7 @@ const ExploreProduct1 = () => {
       imageSrc: "../images/Explore-1.jpg",
       title: "Curology Product Set",
       price: "$500",
+      originalPrice: "$600",
       rating: 4,
       reviews: 145,
     },
@@ -94,7 +118,7 @@ const ExploreProduct1 = () => {
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-5 gap-7">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-7">
         {products.map((product, index) => (
           <ProductCard key={index} {...product} />
         ))}
